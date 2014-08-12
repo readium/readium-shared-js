c9500ba68de914faf9c47d0c81826b33e157651f
Ryan Ackley <ryanackley@gmail.com>
2014-08-12 16:43:37 -0400

    #142 more refactoring

--- js/views/iframe_loader.js
+++ js/views/iframe_loader.js
@@ -26,7 +26,7 @@
 //  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
 //  OF THE POSSIBILITY OF SUCH DAMAGE.
 
-ReadiumSDK.Views.IFrameLoader = function (options) {
+ReadiumSDK.Views.IFrameLoader = function () {
 
     var self = this;
     var eventListeners = {};
