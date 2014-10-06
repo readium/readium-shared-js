047a6f29c314ac24262e5f4fe2c7f3915197cd44
Juan Corona <juanc@evidentpoint.com>
2014-10-06 10:04:11 -0700

    Add initial implementation of a plugins controller.

---
+++ js/controllers/plugins.js
@@ -0,0 +1,189 @@
+//
+//  Created by Mickaël Menu.
+//  Modified by Juan Corona.
+//  Portions adapted from Rangy's Module system: Copyright (c) 2014 Tim Down
+//
+//  Copyright (c) 2014 Readium Foundation and/or its licensees. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without modification,
+//  are permitted provided that the following conditions are met:
+//  1. Redistributions of source code must retain the above copyright notice, this
+//  list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//  this list of conditions and the following disclaimer in the documentation and/or
+//  other materials provided with the distribution.
+//  3. Neither the name of the organization nor the names of its contributors may be
+//  used to endorse or promote products derived from this software without specific
+//  prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+//  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+//  OF THE POSSIBILITY OF SUCH DAMAGE.
+
+//
+// A lightweight plugins controller used to easily add plugins from the host
+// app, eg.
+//
+//   ReadiumSDK.plugins.load(FootnotePlugin, true);
+//
+// The controller will create a new instance of the plugin, forwarding any given
+// arguments. Then, on several useful common Readium notifications, the
+// controller will call predefined callbacks if implemented in the plug-in.
+//
+// Supported callbacks are:
+//      onReaderInitialized()
+//      onDocumentLoadStart($iframe, spineItem)
+//      onDocumentLoadedBeforeInjection($iframe, $document, spineItem);
+//      onDocumentLoaded($iframe, $document, spineItem);
+//
+
+function PluginsController() {
+    this.plugins = [];
+
+    var self = this;
+
+    ReadiumSDK.on(ReadiumSDK.Events.READER_INITIALIZED, function(reader) {
+
+        self.initializePlugins(reader);
+
+        reader.on(ReadiumSDK.Events.CONTENT_DOCUMENT_LOAD_START, function($iframe, spineItem) {
+            var $document = $($iframe[0].contentDocument);
+            self.notifyPlugins("onDocumentLoadStart", $iframe, spineItem)
+        });
+
+        reader.on(ReadiumSDK.Events.CONTENT_DOCUMENT_LOADED_BEFORE_INJECTION, function($iframe, spineItem) {
+            var $document = $($iframe[0].contentDocument);
+            self.notifyPlugins("onDocumentLoadedBeforeInjection", $iframe, $document, spineItem)
+        });
+
+        reader.on(ReadiumSDK.Events.CONTENT_DOCUMENT_LOADED, function($iframe, spineItem) {
+            var $document = $($iframe[0].contentDocument);
+            self.notifyPlugins("onDocumentLoaded", $iframe, $document, spineItem);
+        });
+    });
+};
+
+// Creates a new instance of the given plugin constructor.
+PluginsController.prototype.loadPlugin = function(name, dependencies, options, initFunc) {
+
+    var newPlugin = new Plugin(name, dependencies, options, function(plugin, api) {
+        if (!plugin.initialized) {
+            plugin.initialized = true;
+            try {
+                initFunc(api, plugin);
+                plugin.supported = true;
+            } catch (ex) {
+                var errorMessage = "Module '" + name + "' failed to load: " + getErrorDesc(ex);
+                consoleLog(errorMessage);
+            }
+        }
+    });
+    this.plugins[name] = newPlugin;
+};
+
+// Calls the method <name> on any plugin that implements it, forwarding any
+// given argument.
+PluginsController.prototype.notifyPlugins = function(name) {
+    var args = $.makeArray(arguments);
+    args.shift();
+
+    var self = this;
+    $(this.plugins).each(function(index, plugin) {
+        var callback = plugin[name];
+        if (callback)
+            callback.apply(plugin, args);
+    });
+};
+
+PluginsController.prototype.initializePlugins = function(api){
+    var plugin;
+    for (var pluginName in this.plugins) {
+        if ( (plugin = this.plugins[pluginName]) instanceof Plugin ) {
+            plugin.init(api);
+        }
+    }
+};
+
+ReadiumSDK.Plugins = new PluginsController();
+
+//
+//  The following is adapted from Rangy's Module class:
+//
+//  Copyright (c) 2014 Tim Down
+//
+//  The MIT License (MIT)
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+function Plugin(name, dependencies, options, initializer) {
+    this.name = name;
+    this.dependencies = dependencies;
+    this.options = options;
+    this.initialized = false;
+    this.supported = false;
+    this.initializer = initializer;
+}
+
+Plugin.prototype = {
+    init: function(api) {
+        var requiredPluginNames = this.dependencies || [];
+        for (var i = 0, len = requiredPluginNames.length, requiredPlugin, PluginName; i < len; ++i) {
+            PluginName = requiredPluginNames[i];
+
+            requiredPlugin = Plugins[PluginName];
+            if (!requiredPlugin || !(requiredPlugin instanceof Plugin)) {
+                throw new Error("required Plugin '" + PluginName + "' not found");
+            }
+
+            requiredPlugin.init(api);
+
+            if (!requiredPlugin.supported) {
+                throw new Error("required Plugin '" + PluginName + "' not supported");
+            }
+        }
+
+        // Now run initializer
+        this.initializer(this, api);
+    },
+
+    fail: function(reason) {
+        this.initialized = true;
+        this.supported = false;
+        throw new Error("Plugin '" + this.name + "' failed to load: " + reason);
+    },
+
+    warn: function(msg) {
+        api.warn("Plugin " + this.name + ": " + msg);
+    },
+
+    deprecationNotice: function(deprecated, replacement) {
+        api.warn("DEPRECATED: " + deprecated + " in Plugin " + this.name + "is deprecated. Please use "
+            + replacement + " instead");
+    },
+
+    createError: function(msg) {
+        return new Error("Error in " + this.name + " Plugin: " + msg);
+    }
+};
