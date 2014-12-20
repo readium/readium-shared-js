21ccc8974ff24581381e160e9823850487a50b77
Daniel Weck <daniel.weck@gmail.com>
2014-12-20 18:03:48 +0000

    Media Overlays playback engine, audio seeking fails on Android KitKit (or at least performs poorly, it depends on codecs), so this code checkin gives more time for Android's media backend to synchronise its status, fill the cache, fetch the buffers, etc.

--- js/views/audio_player.js
+++ js/views/audio_player.js
@@ -118,6 +118,12 @@
                 console.debug("Y) timeupdate");
             }
         );
+
+        _audioElement.addEventListener("seeking", function()
+            {
+                console.debug("Z) seeking");
+            }
+        );
     }
 
     /**
@@ -269,7 +275,7 @@
                     {
                         if (DEBUG)
                         {
-                            console.debug("interval timer skipped (still seeking...)");
+//console.debug("interval timer skipped (still seeking...)");
                         }
                                          
                         _intervalTimerSkips++;
@@ -280,15 +286,26 @@
                         }
                         return;
                     }
-    
-                    var currentTime = _audioElement.currentTime;
+                    
+                    var currentTime = undefined;
+                    try
+                    {
+                        currentTime = _audioElement.currentTime;
+                    }
+                    catch (ex)
+                    {
+                        console.error(ex.message);
+                    }
     
     //                if (DEBUG)
     //                {
     //                    console.debug("currentTime: " + currentTime);
     //                }
     
-                    onPositionChanged(currentTime, 1);
+                    if (currentTime)
+                    {
+                        onPositionChanged(currentTime, 1);
+                    }
                 }, 20);
         }
     
@@ -496,15 +513,36 @@
         };
     
         var _readyEvent = _Android ? "canplaythrough" : "canplay";
-        function onReadyToSeek(event)
+        function onReadyToSeek_(event)
         {
-            $(_audioElement).off(_readyEvent, onReadyToSeek);
-            
             if (DEBUG)
             {
                 console.debug("onReadyToSeek #" + event.data.playId);
             }
             playSeekCurrentTime(event.data.seekBegin, event.data.playId, true);
+        }
+        function onReadyToSeek(event)
+        {
+            $(_audioElement).off(_readyEvent, onReadyToSeek);
+            
+            if (!_Android)
+            {
+                onReadyToSeek_(event);
+            }
+            else
+            {
+                if (DEBUG)
+                {
+                    console.debug("onReadyToSeek ANDROID ... waiting a bit ... #" + event.data.playId);
+                }
+                
+                //self.play();
+                playToForcePreload();
+                
+                setTimeout(function() {
+                    onReadyToSeek_(event);
+                }, 1000);
+            }
         }
     
         function playSeekCurrentTime(newCurrentTime, playId, isNewSrc)
@@ -563,7 +601,7 @@
                 }, 5);
             }
         }
-    
+        
         var MAX_SEEK_RETRIES = 10;
         var _seekedEvent1 = _iOS ? "canplaythrough" : "seeked"; //"progress"
         var _seekedEvent2 = _iOS ? "timeupdate" : "seeked";
@@ -619,18 +657,15 @@
                     setTimeout(function()
                     {
                         onSeeked(event);
-                    }, 50);
+                    }, _Android ? 1000 : 200);
                 }
     
                 setTimeout(function()
                 {
+                    _audioElement.pause();
                     try
                     {
-                        _audioElement.pause();
-                        setTimeout(function()
-                        {
-                            _audioElement.currentTime = event.data.newCurrentTime;
-                        }, 0);
+                        _audioElement.currentTime = event.data.newCurrentTime;
                     }
                     catch (ex)
                     {
@@ -640,11 +675,7 @@
                         {
                             try
                             {
-                                _audioElement.pause();
-                                setTimeout(function()
-                                {
-                                    _audioElement.currentTime = event.data.newCurrentTime;
-                                }, 0);
+                                _audioElement.currentTime = event.data.newCurrentTime;
                             }
                             catch (ex)
                             {
