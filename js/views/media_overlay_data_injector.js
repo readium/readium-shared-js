8303a0bc1ebc00aceb0c8246a225c436d4e18a3c
Ric Wright <rkwright@geofx.com>
2014-12-24 12:03:17 -0600

    Inadvertently left the date off the top of the license.

--- js/views/media_overlay_data_injector.js
+++ js/views/media_overlay_data_injector.js
@@ -1,3 +1,5 @@
+//  Copyright (c) 2014 Readium Foundation and/or its licensees. All rights reserved.
+//  
 //  Redistribution and use in source and binary forms, with or without modification, 
 //  are permitted provided that the following conditions are met:
 //  1. Redistributions of source code must retain the above copyright notice, this 
