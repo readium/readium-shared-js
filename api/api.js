d14c7e1928487025d6b91999b195d23acb9cfaed
Ric Wright <rkwright@geofx.com>
2015-03-13 13:35:43 -0500

    Initial commit of the API docs to the new location in readium-shared-js

---
+++ api/api.js
@@ -0,0 +1,14 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ReadiumSDK.Models.BookmarkData",
+        "ReadiumSDK.Models.CurrentPagesInfo",
+        "ReadiumSDK.Models.Package",
+        "ReadiumSDK.Models.PackageData",
+        "ReadiumSDK.Models.Spine",
+        "ReadiumSDK.Models.SpineItem"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
