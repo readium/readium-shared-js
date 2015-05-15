8710a2449107b7cdd8c364460108ddd9c9edc095
Dmitry M <dmitrym@evidentpoint.com>
2015-05-15 09:26:34 -0700

    Attempt to hide the inactive views in order to solve the duplicate rendering problem

--- js/views/reader_view.js
+++ js/views/reader_view.js
@@ -270,6 +270,7 @@ var ReaderView = function (options) {
             cachedView.setViewSettings(_viewerSettings);
             cachedView.openPage(openPageRequest,2);
             cachedView.setCached(true);
+            cachedView.hide();
         }
         return cachedView;
     };
@@ -303,7 +304,9 @@ var ReaderView = function (options) {
                         console.log('%c View loaded in the background...%d cached views', 'background: grey; color: blue', _cachedViews.length);
                         _cachedViews.push(view);    
                     } else {
-                        console.debug('%c Prevented from loading duplicate view...%d cached views', 'background: grey; color: red', _cachedViews.length);                        
+                        console.debug('%c Prevented from loading duplicate view...%d cached views', 'background: grey; color: red', _cachedViews.length);
+                        view.hide();
+                        delete view;
                     }
                 })
             };
@@ -392,6 +395,11 @@ var ReaderView = function (options) {
         setTimeout(function () {
 
             callback(true);
+            _.each(_cachedViews, function(view) {
+                if (view !== _currentView) {
+                    view.hide();
+                }
+            });
 
         }, 50);
 
