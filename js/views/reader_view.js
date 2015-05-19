db1921b957aa605e7bb3af9770da7b3a7f54eca9
Dmitry M <dmitrym@evidentpoint.com>
2015-05-19 12:51:04 -0700

    Make sure that previous spine item is valid (eg: at the beginning of the book) before loading it

--- js/views/reader_view.js
+++ js/views/reader_view.js
@@ -277,6 +277,7 @@ var ReaderView = function (options) {
 
     // returns true is view changed
     function initViewForItem(spineItem, callback) {
+        console.log('%c Init view for item', 'background: black; color: red');
         if (_currentView) {
             _currentView.hide();
             _currentView.setCached(true);
@@ -305,7 +306,9 @@ var ReaderView = function (options) {
                         _cachedViews.push(view);    
                     } else {
                         console.debug('%c Prevented from loading duplicate view...%d cached views', 'background: grey; color: red', _cachedViews.length);
+                        console.debug("fixed-book-frame:", $('.fixed-book-frame', _$el).length);
                         view.hide();
+                        view.remove();
                         delete view;
                     }
                 })
@@ -314,12 +317,12 @@ var ReaderView = function (options) {
             var rightSpineItemToPrefetch = _spine.items[spineItem.index + spinesWithinTheCurrentView];
             var leftSpineItemToPrefetch = _spine.items[spineItem.index - spinesWithinTheCurrentView];
 
-            if (rightSpineItemToPrefetch.index < _spine.items.length && _.isUndefined(getCachedViewForSpineItem(rightSpineItemToPrefetch))) {
+            if (rightSpineItemToPrefetch && _.isUndefined(getCachedViewForSpineItem(rightSpineItemToPrefetch))) {
                 var rightView = createPrefetchedViewForSpineItemIndex(rightSpineItemToPrefetch.index);
                 saveViewOnceLoaded(rightView);
             }
 
-            if (leftSpineItemToPrefetch.index > 0 && _.isUndefined(getCachedViewForSpineItem(leftSpineItemToPrefetch))) {
+            if (leftSpineItemToPrefetch && _.isUndefined(getCachedViewForSpineItem(leftSpineItemToPrefetch))) {
                  var leftView = createPrefetchedViewForSpineItemIndex(leftSpineItemToPrefetch.index, "last");
                  saveViewOnceLoaded(leftView);
             }
