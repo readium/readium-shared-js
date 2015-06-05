90b08d308ccceaf6462bd129c4b800044ccc9cf3
Juan Corona <juanc@evidentpoint.com>
2015-06-04 20:29:31 -0700

    Stub out files that will be generated by the plugins watcher

--- build-config/RequireJS_config_plugins.js
+++ build-config/RequireJS_config_plugins.js
@@ -1,1 +1,25 @@
 //Do not modify this file, it is automatically generated.
+require.config({
+  packages: [
+    {
+      name: "readium_shared_js",
+      location: process._RJS_rootDir(1) + '/js',
+
+      main: "globalsSetup"
+    },
+
+    {
+      name: "readium_plugin_annotations",
+      location: process._RJS_rootDir(1) + '/plugins/annotations',
+
+      main: "main"
+    },
+
+    {
+      name: "readium_plugin_example",
+      location: process._RJS_rootDir(1) + '/plugins',
+
+      main: "example"
+    }
+  ]
+});
