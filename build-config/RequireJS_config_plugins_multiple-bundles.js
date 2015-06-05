936cf23dced6a78737cd5ffb9da801d9395d9e94
Juan Corona <juanc@evidentpoint.com>
2015-06-04 22:57:16 -0700

    Test out plugin config generation. Move example plugin files to match a convention.

--- build-config/RequireJS_config_plugins_multiple-bundles.js
+++ build-config/RequireJS_config_plugins_multiple-bundles.js
@@ -1,21 +1,21 @@
 //Do not modify this file, it is automatically generated.
-require.config({
-    modules:
-    [
-        {
-            name: "readium-plugin-example",
-            create: true,
-            include: ["readium_plugin_example"],
-            exclude: ["readium-external-libs", "readium-cfi-js", "readium-shared-js"],
-            insertRequire: ["readium_plugin_example"]
-        },
-
-        {
-            name: "readium-plugin-annotations",
-            create: true,
-            include: ["readium_plugin_annotations"],
-            exclude: ["readium-external-libs", "readium-cfi-js", "readium-shared-js"],
-            insertRequire: ["readium_plugin_annotations"]
-        }
-    ]
-});
+/*#TEMPLATE#
+name: {"readium-plugin-{{NAME}}"},
+create: true,
+include: ["readium_plugin_{{NAME}}"],
+exclude: ["readium-external-libs", "readium-cfi-js", "readium-shared-js"],
+insertRequire: ["readium_plugin_{{NAME}}"]
+#TEMPLATE#*/
+require.config({modules:[{
+name: "readium-plugin-annotations",
+create: true,
+include: ["readium_plugin_annotations"],
+exclude: ["readium-external-libs", "readium-cfi-js", "readium-shared-js"],
+insertRequire: ["readium_plugin_annotations"]
+},{
+name: "readium-plugin-example",
+create: true,
+include: ["readium_plugin_example"],
+exclude: ["readium-external-libs", "readium-cfi-js", "readium-shared-js"],
+insertRequire: ["readium_plugin_example"]
+}]});
--- build-config/RequireJS_config_plugins_single-bundle.js
+++ build-config/RequireJS_config_plugins_single-bundle.js
@@ -1,5 +1,6 @@
 //Do not modify this file, it is automatically generated.
-require.config({
-    include: ["readium_plugin_example", "readium_plugin_annotations"],
-    insertRequire: ["readium_plugin_example", "readium_plugin_annotations"]
+require.config({include:
+["readium_plugin_example", "readium_plugin_annotations"]
+,insertRequire:
+["readium_plugin_example", "readium_plugin_annotations"]
 });
