8dfe569b0ce2e43ecc9b38f0ae687f183ffc8894
build <build@dnjplatbuild03.barnesandnoble.com>
2017-05-05 04:23:27 -0400

    NO-JIRA: commit new generated JS files, 0.24.0.4

--- build-output/epubReadingSystem.js
+++ build-output/epubReadingSystem.js
@@ -1,7 +1,7 @@
 
-ReadiumSDK.READIUM_SHARED_JS_sha = 'f29640c1767b629ab4f0f1448eceef9ada83fb89';
-ReadiumSDK.READIUM_SHARED_JS_tag = 'readium-js-0.14.0.2-4-gf29640c';
+ReadiumSDK.READIUM_SHARED_JS_sha = '5ce9d98b45fb482578064de18f966894cd9629a1';
+ReadiumSDK.READIUM_SHARED_JS_tag = 'readium-js-0.24.0.3-3-g5ce9d98';
 ReadiumSDK.READIUM_SHARED_JS_clean = false;
 ReadiumSDK.READIUM_SHARED_JS_branch = 'dev';
-ReadiumSDK.READIUM_SHARED_JS_timestamp = 1493970695000;
-ReadiumSDK.READIUM_dateTimeString = 'Fri May  5 03:51:35 EDT 2017';
+ReadiumSDK.READIUM_SHARED_JS_timestamp = 1493972605000;
+ReadiumSDK.READIUM_dateTimeString = 'Fri May  5 04:23:25 EDT 2017';
