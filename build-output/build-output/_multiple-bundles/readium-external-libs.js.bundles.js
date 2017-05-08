efa81df5ba7ee99b88e778e39c3682c3004bb8a2
build <build@dnjplatbuild03.barnesandnoble.com>
2017-05-08 02:08:02 -0400

    NO-JIRA: commit new generated JS files, 0.24.0.5

---
+++ build-output/build-output/_multiple-bundles/readium-external-libs.js.bundles.js
@@ -0,0 +1,1 @@
+require.config({bundles: {"readium-external-libs":["FontLoader","IPv6","SecondLevelDomains","URIjs","console_shim","cssom","domReady","eventEmitter","jquery","jquerySizes","punycode","rangy","rangy-core","rangy-cssclassapplier","rangy-highlighter","rangy-position","rangy-textrange","underscore"]}});
---
+++ build-output/build-output/epubReadingSystem.js
@@ -0,0 +1,7 @@
+
+ReadiumSDK.READIUM_SHARED_JS_sha = '5ce9d98b45fb482578064de18f966894cd9629a1';
+ReadiumSDK.READIUM_SHARED_JS_tag = 'readium-js-0.24.0.3-3-g5ce9d98';
+ReadiumSDK.READIUM_SHARED_JS_clean = false;
+ReadiumSDK.READIUM_SHARED_JS_branch = 'dev';
+ReadiumSDK.READIUM_SHARED_JS_timestamp = 1493972605000;
+ReadiumSDK.READIUM_dateTimeString = 'Fri May  5 04:23:25 EDT 2017';
