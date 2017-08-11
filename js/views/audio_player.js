471fbe16851751b40e73771c7f8bf8b11c525c67
Zak Hsieh <zhsieh@book.com>
2017-08-11 15:35:21 +0800

    IOS-13113: Change onSeeked callback to event 'seeked'

--- js/views/audio_player.js
+++ js/views/audio_player.js
@@ -72,7 +72,7 @@ define(['jquery'],function($) {
 
             _audioElement.addEventListener("loadedmetadata", function()
                 {
-                    console.debug("3) loadedmetadata");
+                    console.debug("3) loadedmetadata: duration = " + _audioElement.duration);
                 }
             );
 
@@ -605,7 +605,7 @@ define(['jquery'],function($) {
         }
         
         var MAX_SEEK_RETRIES = 10;
-        var _seekedEvent1 = _iOS ? "canplaythrough" : "seeked"; //"progress"
+        var _seekedEvent1 = "seeked";
         var _seekedEvent2 = _iOS ? "timeupdate" : "seeked";
         function onSeeked(event)
         {
