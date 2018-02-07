972ad028393b0ad05a8d0a4a0a253d396c408ba0
Juan Corona <juanc@evidentpoint.com>
2018-02-06 18:02:58 -0800

    Add initial typings file

---
+++ dev/readium-shared-js.d.ts
@@ -0,0 +1,8 @@
+declare module 'readium-shared-js' {
+  export var ReaderView: any;
+  export var ReflowableView: any;
+  export var Package: any;
+  export var ViewerSettings: any;
+  export var StyleCollection: any;
+  export default ReaderView;
+}
