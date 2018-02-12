af31561e7b711589207a527d19e1457350c1730d
Juan Corona <juanc@evidentpoint.com>
2018-02-12 02:47:27 -0800

    Support source map generation for UMD bundle output!

--- readium-build-tools/RequireJS_function_umd-bundle_complete.js
+++ readium-build-tools/RequireJS_function_umd-bundle_complete.js
@@ -13,34 +13,88 @@
 
 function (data) {
     console.log("========> onModuleBundleComplete [amdclean]");
+
+    console.log('Generating AMD cleaned bundle...');
+
     var fs = module.require('fs');
     var path = module.require('path');
     var rimraf = module.require('rimraf');
+    var sorcery = module.require('sorcery');
+    var Concat = module.require('concat-with-sourcemaps');
+    var amdclean = module.require('amdclean');
+
     var outputPath = path.dirname(data.path);
     var outputFileName = path.basename(data.path);
     var rootPath = path.resolve(outputPath, '../../');
-
+    
     var configDir = process.cwd()
         + "/"
         + process._RJS_Path_RelCwd__ConfigDir;
-
-    var amdclean = module.require('amdclean');
-    var outputFile = data.path;
-    var cleanedCode = amdclean.clean({
-        'wrap': true,
-        'filePath': outputFile,
+    
+    var codePath = data.path;
+    
+    var mapPath = codePath + '.map';
+    var map = fs.readFileSync(mapPath, 'utf8');
+    var mapJSON = JSON.parse(map);
+    mapJSON.sourceRoot = '../../';
+    map = JSON.stringify(mapJSON);
+    fs.writeFileSync(mapPath, map);
+    var code = fs.readFileSync(codePath, 'utf8');
+    var cleaned = amdclean.clean({
+        'sourceMap': map,
+        'code': fs.readFileSync(codePath, 'utf8'),
+        'wrap': false, // do not use wrap together with escodegen.sourceMapWithCode since it breaks the logic
         'prefixTransform': function (postNormalizedModuleName, preNormalizedModuleName) {
             return '_' + postNormalizedModuleName;
+        },
+        'esprima': {
+            'source': outputFileName // name of your file to appear in sourcemap
+        },
+        'escodegen': {
+            'sourceMap': true,
+            'sourceMapWithCode': true
         }
     });
-    cleanedCode = fs.readFileSync(configDir + '/umd_wrapper_start.template') +
-        '\n' + cleanedCode + '\nreturn _' + data.name.replace(/-/g, '_') + ';\n' +
-        fs.readFileSync(configDir + '/umd_wrapper_end.template');
+    console.log("AMD cleaned bundle generated.");
+    
     var cleanedOutputPath = path.resolve(outputPath, '../_umd-bundle');
+    rimraf.sync(cleanedOutputPath);
     fs.mkdirSync(cleanedOutputPath);
-    fs.writeFileSync(path.join(cleanedOutputPath, outputFileName), cleanedCode);
-    //fs.unlinkSync(outputFile + '.map'); // Delete the source map, as it is incompatible :(
+    
+    
+    var newCodePath = path.join(cleanedOutputPath, outputFileName);
+    var newMapPath = path.join(cleanedOutputPath, outputFileName + '.map');
+    var newCode = cleaned.code;
+    cleaned.map.sourceRoot = '../../';
+    var newMap = cleaned.map.toString();
+    
+    var concat = new Concat(true, newCodePath, '\n');
+    concat.add(null, fs.readFileSync(configDir + '/umd_wrapper_start.template'));
+    concat.add(null, newCode, newMap);
+    concat.add(null, 'return _' + data.name.replace(/-/g,'_') + ';');
+    concat.add(null, fs.readFileSync(configDir + '/umd_wrapper_end.template'));
+    concat.add(null, '//# sourceMappingURL=' + outputFileName + '.map');
+    
+    var concatenatedContent = concat.content.toString();
+    var sourceMapForContent = concat.sourceMap;
+    
+    fs.writeFileSync(newCodePath, concatenatedContent);
+    fs.writeFileSync(newMapPath, sourceMapForContent);
+    
     rimraf.sync(outputPath); // Delete the original path as it was temporary
-
+    
     console.log("UMD bundle created.");
+    var pathTo =  path.join(cleanedOutputPath, outputFileName);
+    var chain = sorcery.loadSync(pathTo, {
+        content: {
+            [newCodePath]: concatenatedContent,
+            [codePath]: code,
+        },
+        sourcemaps: {
+            [newMapPath]: sourceMapForContent,
+            [mapPath]: map
+        }
+    });
+    chain.writeSync(newCodePath);
+    console.log("UMD source map created.");
 }
